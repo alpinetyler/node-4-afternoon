7fc05b78f54843a463e5f909439ce582df4e23d5
TylerT <alpinetyler@gmail.com>
2019-06-11 15:42:50 -0600

    working commit

---
+++ server/index.js
@@ -0,0 +1,46 @@
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session")
+
+
+const checkForSession = require('./middlewares/checkForSession');
+
+const swagController = require('./controllers/swagController');
+const authController = require('./controllers/authController')
+const cartController = require('./controllers/cartController')
+const searchController = require('./controllers/searchController')
+
+
+require('dotenv').config()
+const app = express();
+
+const {SERVER_PORT, SESSION_SECRET} = process.env
+
+app.use(express.json())
+
+app.use(session({
+   secret: SESSION_SECRET, 
+   resave: false,
+   saveUninitialized: false
+   
+}))
+
+app.post('/api/register', authController.register);
+app.post('/api/login', authController.login);
+app.post('/api/signout', authController.signout);
+app.get('api/user', authController.getUser);
+
+app.get("/api/swag", swagController.read);
+
+app.post('/api/cart/checkout', cartController.checkout);
+app.post('/api/cart/:id', cartController.add);
+app.delete('/api/cart/:id', cartController.delete);
+
+app.get("/api/search", searchController.search)
+
+
+
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Our server is listening on port ${SERVER_PORT}`)
+})
